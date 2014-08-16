f1e200447f3596c26ad361882d30f5786d853b32
wssbck <michal@gancarski.pl>
2014-08-16 19:11:38 +0200

    Work on charts

--- app/static/js/graphs.js
+++ app/static/js/graphs.js
@@ -1,42 +1,56 @@
 // charts
 
 var
-	berlinChart = dc.geoChoroplethChart("#s1"),
-	berlinChart = dc.barChart("#s1"),
-	chart2 = dc.bubbleChart("#s2"),
+	//berlinChart = dc.geoChoroplethChart("#s1"),
+	chart1 = dc.barChart("#s1"),
+	chart2 = dc.pieChart("#s2"),
 	chart3 = dc.bubbleChart("#s3"),
 	chart4 = dc.bubbleChart("#s4"),
 	chart5 = dc.bubbleChart("#s5"),
 	chart6 = dc.bubbleChart("#s6"),
 	chart7 = dc.bubbleChart("#s7"),
-	chart8 = dc.bubbleChart("#s8"),
-	chart9 = dc.bubbleChart("#s9");
+	chart8 = dc.bubbleChart("#s8");
 
 d3.json("/js/data.json", function (error, json) {
 	var data = crossfilter(json);
 
 	var dimDistrict = data.dimension(function(d){
-		return d.district;
+		return d.quarter;
 	});
 	var groupDistrictPropertyCount = dimDistrict.group().reduceCount();
 
 	//d3.json("/js/us-states.json", function (error, berlinJson) {
-		berlinChart
+		chart1
 			//.width(990)
-			.height(200)
+			.height(300)
                 	.dimension(dimDistrict)
                 	.group(groupDistrictPropertyCount)
                 	//.overlayGeoJson(berlinJson.features, "district", function(d) {
-                       // 	return d.properties.Name;
+                        //	return d.properties.Name;
 			//})
-                    	.title(function (d) {
-                        	return "Title";
-                    	})
 			.x(d3.scale.ordinal())
 			.xUnits(dc.units.ordinal)
+			.title(function (d) {
+			    return d.value;
+			})
+			//.xAxis().ticks(4)
 			.elasticX(true)
-			.elasticY(true)
+			.render();
+		chart2
+			//.width(990)
+			.height(300)
+                	.dimension(dimDistrict)
+                	.group(groupDistrictPropertyCount)
+                	//.overlayGeoJson(berlinJson.features, "district", function(d) {
+                        //	return d.properties.Name;
+			//})
+			//.x(d3.scale.ordinal())
+			//.xUnits(dc.units.ordinal)
+			.title(function (d) {
+			    return d.value;
+			})
+			//.xAxis().ticks(4)
+			//.elasticX(true)
 			.render();
 	//});
-	console.log(berlinChart.group().top(1));
 });
